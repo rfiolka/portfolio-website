27ef68a098cba0615673078a77e44c2c3eee1183
rfiolka <roksana.fiolka@icloud.com>
2022-11-09 13:29:21 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Experience from './pages/Experience'
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
 
 
 import './App.css';
@@ -17,6 +19,7 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/experience" element={<Experience />} />
       </Routes>
+      <Footer />
     </Router>
     </div>
   );
