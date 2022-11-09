95ee47fb4a2e79c509e0d14770c6bf395fdcd538
rfiolka <roksana.fiolka@icloud.com>
2022-11-09 13:20:40 +0100

    added navbar [Navbar.js]

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Experience from './pages/Experience'
+import Navbar from "./components/Navbar"
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
