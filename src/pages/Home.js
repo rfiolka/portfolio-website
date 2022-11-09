1e134684e7f8ead14e64ba7d8a01eea863789491
rfiolka <roksana.fiolka@icloud.com>
2022-11-09 13:55:58 +0100

    added homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import EmailIcon from '@material-ui/icons/Email'
+import GithubIcon from '@material-ui/icons/GitHub'
+import "../styles/Home.css"
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home"> 
+    <div className="about">
+      <h2>Hi, my name is Roksana</h2>
+      <div className='prompt'> <p>Recent university graduate with B.S in Computer Science with a passion for personal growth and for software development</p>
+    <LinkedInIcon />
+    <EmailIcon />
+    <GithubIcon />
+    </div>
+    </div>
+    <div className='skills'>
+      <h1>Skills</h1>
+      <ol className='list'>
+        <li className='item'> 
+        <h2>Front-End</h2>
+        <span>ReactJS, HTML, CSS, NPM</span>
+        </li>
+        <li className='item'> 
+        <h2>Languages</h2>
+        <span>JavaScript, TypeScript</span>
+        </li>
+      </ol>
+    </div>
+    </div>
   )
 }
 
